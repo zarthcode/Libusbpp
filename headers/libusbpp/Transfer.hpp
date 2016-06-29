d6e6f5b1e9b648b9585555d12f7f4d1af24e791a
Stephan Linz <linz@li-pro.net>
2016-06-29 11:54:42 +0200

    add missing public statement in transfer interface
    Error was:
    
    .../src/endpoint_impl.cpp:130:109:   required from here
    /usr/include/c++/4.8/bits/shared_ptr_base.h:963:64: error:
       ‘std::enable_shared_from_this<LibUSB::Transfer>’ is an
       inaccessible base of ‘LibUSB::ControlTransfer’
    
    .../src/endpoint_impl.cpp:144:111:   required from here
    /usr/include/c++/4.8/bits/shared_ptr_base.h:963:64: error:
       ‘std::enable_shared_from_this<LibUSB::Transfer>’ is an
       inaccessible base of ‘LibUSB::InterruptTransfer’
    
    .../src/endpoint_impl.cpp:157:106:   required from here
    /usr/include/c++/4.8/bits/shared_ptr_base.h:963:64: error:
       ‘std::enable_shared_from_this<LibUSB::Transfer>’ is an
       inaccessible base of ‘LibUSB::BulkTransfer’
    
    .../src/endpoint_impl.cpp:168:113:   required from here
    /usr/include/c++/4.8/bits/shared_ptr_base.h:963:64: error:
       ‘std::enable_shared_from_this<LibUSB::Transfer>’ is an
       inaccessible base of ‘LibUSB::IsochronousTransfer’
    
    Signed-off-by: Stephan Linz <linz@li-pro.net>

--- headers/libusbpp/Transfer.hpp
+++ headers/libusbpp/Transfer.hpp
@@ -33,7 +33,7 @@ namespace LibUSB
 	class TransferImpl;
 
 	/// USB Data transfer object.
-	class Transfer : std::enable_shared_from_this<Transfer>
+	class Transfer : public std::enable_shared_from_this<Transfer>
 	{
 
 	public:
